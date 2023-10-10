ec5cd3dfbf009c037cf5367f7307fb4923debb52
zrx <201811210137@mail.bnu.edu.cn>
2023-10-10 18:02:02 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples (applenum: i32) -> i32 {
+    if applenum > 40 {
+        applenum
+    } else {
+        2 * applenum
+    }
+}
 
 // Don't modify this function!
 #[test]
